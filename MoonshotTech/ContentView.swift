a7db8fffd3767f4ca518309893292dd564a89d8e
Waihon Yew <yewwaihon@gmail.com>
2021-06-27 15:50:22 +0800

    Initial Commit

---
+++ MoonshotTech/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MoonshotTech
+//
+//  Created by Waihon Yew on 27/06/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
