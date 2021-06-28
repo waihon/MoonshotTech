a10a3d5ef07cdc69ed7d19f99a3633cec6c23d4b
Waihon Yew <yewwaihon@gmail.com>
2021-06-29 00:50:38 +0800

    Create a CustomText to demonstrate that when we add views to a scroll view that get created immediately

--- MoonshotTech/ContentView.swift
+++ MoonshotTech/ContentView.swift
@@ -20,12 +20,25 @@ struct GeometryReaderView: View {
   }
 }
 
+struct CustomText: View {
+  var text: String
+  
+  var body: some View {
+    Text(text)
+  }
+  
+  init(_ text: String) {
+    print("Creating a new CustomText")
+    self.text = text
+  }
+}
+
 struct ContentView: View {
   var body: some View {
     ScrollView(.vertical) {
       VStack(spacing: 10) {
         ForEach(0..<100) {
-          Text("Item \($0)")
+          CustomText("Item \($0)")
             .font(.title)
         }
       }
