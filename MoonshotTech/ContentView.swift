31913960d4e654c55522aeaca6b1791ab02cb2a9
Waihon Yew <yewwaihon@gmail.com>
2021-06-28 23:56:35 +0800

    Add a ScrollView with 100 Text views

--- MoonshotTech/ContentView.swift
+++ MoonshotTech/ContentView.swift
@@ -20,6 +20,20 @@ struct GeometryReaderView: View {
   }
 }
 
+struct ContentView: View {
+  var body: some View {
+    ScrollView(.vertical) {
+      VStack(spacing: 10) {
+        ForEach(0..<100) {
+          Text("Item \($0)")
+            .font(.title)
+        }
+      }
+    }
+  }
+}
+
+
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
     ContentView()
