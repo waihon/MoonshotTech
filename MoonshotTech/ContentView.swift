fe128df46c8d65059d04291496ec7600870ae3f7
Waihon Yew <yewwaihon@gmail.com>
2021-07-02 01:02:32 +0800

    Convert JSON string to the Data type, then decode that into a User instance

--- MoonshotTech/ContentView.swift
+++ MoonshotTech/ContentView.swift
@@ -94,7 +94,11 @@ struct ContentView: View {
       }
       """
       
-      // More code to come
+      let data = Data(input.utf8)
+      let decoder = JSONDecoder()
+      if let user = try? decoder.decode(User.self, from: data) {
+        print(user.address.street)
+      }
     }
   }
 }
